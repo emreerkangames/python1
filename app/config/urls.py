677e423430ce1ea1df75f02ddea62a3637045a73
Emre Erkan <emreerkanbilgi2@gmail.com>
2025-03-02 16:49:27 +0300

    Add SVG icons and JavaScript files for admin interface; update settings and URLs for static file handling

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+# Statik dosyaların doğru şekilde sunulması için
+if settings.DEBUG:  # Sadece geliştirme ortamında aktif edilir
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
