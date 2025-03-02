7db328ddafe1a19865d208a893ddc1e1195021e9
Emre Erkan <emreerkanbilgi2@gmail.com>
2025-03-02 14:50:18 +0300

    Update ALLOWED_HOSTS to allow all hosts in development

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)k(b#0a@zfg01bt#%o93(e*zz-rb^&f+2_^)oxogcmyl+kcpj5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
